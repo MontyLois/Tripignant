efd7b5f841042fa0ffb81db0e3af4afd902b8970
MontyLois <lois.monty@outlook.fr>
2024-12-10 12:12:26 +0100

    object slide somewhat correctly (false)
    fix object movable script. At least it respect the axis, but not the direction.........

--- Assets/Scripts/InteractionSystem/MovableObject.cs
+++ Assets/Scripts/InteractionSystem/MovableObject.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 namespace PhotoSystem
 {
@@ -11,9 +12,12 @@ namespace PhotoSystem
         
         public void Move(Transform playerPosition, float direction)
         {
+            Vector3 moveDirection = Vector3.zero;
+            
+            /*
+            Test(playerPosition);
             //object position to player position
             Vector3 objectToPlayer = playerPosition.position - transform.position;
-            Vector3 moveDirection = Vector3.zero;
             //calculate the relative direction to move the object
             Vector3 relativdir = playerPosition.forward * direction;
             
@@ -27,8 +31,40 @@ namespace PhotoSystem
                 moveDirection = new Vector3(0, 0, relativdir.z).normalized;
             }
             
+            */
+            
+            
+            //let's try something new
+            
+            //we are calculating the dot product between the player forward and the object forward 
+            //to determin on wich axis we should move the object
+            float dotproduct = Vector3.Dot(playerPosition.forward, this.transform.forward);
+            float absolute = Math.Abs(dotproduct);
+            //if it's under 0.5, then the player is align with the z axis of the object
+            if (absolute < 0.5)
+            {
+                moveDirection = this.transform.right;
+            }
+            //if it's more than 0.5, then the player is align with the x axis of the object
+            else
+            {
+                moveDirection = this.transform.forward;
+            }
+
+            //direction : if we are pulling (-1) or pushin (1)
+            //dotproduct : neg if we are in front or on the righe, pos if we are behind or on the left.
+            moveDirection = moveDirection * direction * dotproduct;
+            moveDirection = moveDirection.normalized;
+            
             // Move the object
-            rb.AddForce(moveDirection* moveSpeed);
+            rb.AddForce(moveDirection * moveSpeed);
+        }
+
+        public void Test(Transform playerPosition)
+        {
+            float dotproduct = Vector3.Dot(playerPosition.forward, this.transform.forward);
+            float absolute = Math.Abs(dotproduct);
+            Debug.Log(dotproduct);
         }
     }
 }
