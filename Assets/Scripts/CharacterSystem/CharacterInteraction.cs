fffb26baab3152588caf9a34c8795fdfcdeb5b6c
MontyLois <lois.monty@outlook.fr>
2024-12-16 22:24:11 +0100

    brand new pulley system c:
    now we can add our last puzzle
    the weight stacking look a bit weird buuuut it works !

--- Assets/Scripts/CharacterSystem/CharacterInteraction.cs
+++ Assets/Scripts/CharacterSystem/CharacterInteraction.cs
@@ -1,4 +1,5 @@
 using System;
+using InterractionSystem.Game;
 using PhotoSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,6 +11,7 @@ namespace CharacterSystem.Game.CharacterSystem
         // Reference to the item in the player's hand
         private IUsable itemInHand;
         private IMovable currentMovableObject;
+        private GameObject weight;
         
         [SerializeField]
         private Camera playerCamera;
@@ -17,6 +19,8 @@ namespace CharacterSystem.Game.CharacterSystem
         private float interactionRange;
         [SerializeField]
         private Transform itemHolder;
+        [SerializeField]
+        private Transform secondItemHolder;
 
 
         private float movingInput;
@@ -77,6 +81,22 @@ namespace CharacterSystem.Game.CharacterSystem
                             
                         }
                         itemInHand = usable;
+                    }
+
+                    IStackable stackable = hitInfo.collider.GetComponent<IStackable>();
+                    if (stackable!=null)
+                    {
+                        Debug.Log("we are stacking baby");
+                        if (weight != null)
+                        {
+                            stackable.AddWeight(weight);
+                            weight = null;
+                        }
+                        else
+                        {
+                            weight = stackable.RemoveWeight();
+                            weight.transform.SetParent(secondItemHolder);
+                        }
                     }
                 }
             }
---
+++ Assets/Scripts/InteractionSystem/Pulley.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InterractionSystem.Game
+{
+    public class Pulley : MonoBehaviour, IStackable
+    { 
+        [SerializeField]
+        private float height_max;
+        private float height;
+        [SerializeField]
+        private int nb_weight_max;
+        private int nb_weight=0;
+        [SerializeField] 
+        private Transform stageSet;
+        
+       public void AddWeight(GameObject weight)
+       {
+           //set new parent
+           weight.transform.SetParent(this.transform);
+           
+           //stack the child on top of the others.
+           float gap = 0;
+           Collider collider = weight.GetComponent<Collider>();
+           if (collider)
+           {
+               gap = collider.bounds.size.y;
+           }
+           float nbchild = this.transform.childCount;
+           float position_y = gap * (nbchild-1);
+           Vector3 position = new Vector3(0,position_y,0);
+           weight.transform.localPosition = position;
+           weight.transform.localRotation = Quaternion.identity;
+           
+           //add weight to the pulley and move the stage set
+           nb_weight++;
+           ChangeHeight();
+       }
+
+       public GameObject RemoveWeight()
+       {
+           nb_weight--;
+           ChangeHeight();
+           //ensure LIFO 
+           int child_number = this.transform.childCount -1;
+           if (child_number < 0)
+           {
+               return null;
+           }
+           GameObject weight = this.transform.GetChild(child_number).gameObject;
+           return weight;
+       }
+
+       private void ChangeHeight()
+       {
+           //calculate how much the number of weight will lift up the stage set
+           float ratio = (float) nb_weight / nb_weight_max;
+           //cap to 1
+           if (ratio > 1)
+           {
+               ratio = 1;
+           }
+           //calculate the height
+           height = ratio * height_max;
+           Debug.Log("ratio :"+ ratio+" heigth :"+ height);
+           //set new height
+           if (stageSet)
+           {
+               Vector3 position = new Vector3(stageSet.position.x, height, stageSet.position.z);
+               stageSet.SetPositionAndRotation(position, stageSet.rotation);
+           }
+       }
+    }
+}
